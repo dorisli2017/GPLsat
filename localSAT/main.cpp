cbc7ec3774773a1964802b23ed8553ef058c63c6
dorisli2017 <ailixiaod@gmail.com>
2018-05-08 15:18:53 +0200

    add the local search eclipse project

---
+++ localSAT/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 8, 2018
+ *      Author: ping
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
+
+
